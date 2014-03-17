55a5acb212fcf4fda5bc36245e40058b34037a18
ValerioBocchi <valerio.bocchi@gmail.com>
2014-03-17 13:17:30 +0100

    prova3

--- script/camera.js
+++ script/camera.js
@@ -4,7 +4,7 @@
 ;(function($){$.fn.camera = function(opts, callback) {
 	
 	var defaults = {
-		alignment			: 'center', //topLeft, topCenter, topRight, centerLeft, center, centerRight, bottomLeft, bottomCenter, bottomRight   prova pippo
+		alignment			: 'center', //topLeft, topCenter, topRight, centerLeft, center, centerRight, bottomLeft, bottomCenter, bottomRight   prova
 		
 		
 		autoAdvance			: true,	//true, false
